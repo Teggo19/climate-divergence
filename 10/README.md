317bfedce6df302f42a782886fdad25882254d73
martinsw01 <martin.s.winther@gmail.com>
2024-11-09 09:40:49 +0100

    - Refactored code - Dont flip x when flipping f and A - Describe new, simpler theory applied in the code

--- 10/README.md
+++ 10/README.md
@@ -32,35 +32,35 @@ $$
 
 ## Discretization
 
-We can now use the finite difference operators to discretize the equation:
+We can now use the finite difference operators to discretize the equation in the interior:
 
-$$-D \mathcal L_x \left[ (1-x^2) \mathcal L_x T \right]_j = -I_j + Q S_j a_j,$$
+$$-D \delta_x \left[ (1-x^2) \delta_x T \right]_j = -I_j + Q S_j a_j$$
 
-where $\mathcal L_x \in \{ \delta_x, \Delta_x, \nabla_x \}$. In the interior, we use $\mathcal L_x = \delta_x$:
+This results in the equations
 
-$$-D \left(\frac{1-x_{j+1/2}^2}{\Delta x} \frac{T_{j+1}-T_j}{\Delta x} - \frac{1-x_{j-1/2}^2}{\Delta x} \frac{T_j-T_{j-1}}{\Delta x}\right) = -I_j + Q S_j a_j$$
+$$-D \left(\frac{1-x_{j+1/2}^2}{\Delta x} \frac{T_{j+1}-T_j}{\Delta x} - \frac{1-x_{j-1/2}^2}{\Delta x} \frac{T_j-T_{j-1}}{\Delta x}\right) = -I_j + Q S_j a_j,$$
 
-On the left boundary (equator or ice cap), we use $\mathcal L_x = \Delta_x$:
-
-$$-D\left(\frac{1-x_1^2}{\Delta x} \frac{T_2-T_1}{\Delta x} - \frac{1-x_0^2}{\Delta x} \frac{T_1-T_0}{\Delta x} \right) = -I_0 + Q S_0 a_0$$
-
-On the right boundary (ice cap or pole), we use $\mathcal L_x = \nabla_x$:
-
-$$-D\left(\frac{1-x_N^2}{\Delta x} \frac{T_N-T_{N-1}}{\Delta x} - \frac{1-x_{N-1}^2}{\Delta x} \frac{T_{N-1}-T_{N-2}}{\Delta x} \right) = -I_N + Q S_N a_N$$
+for $j=1, 2, \ldots, N-1$. On the boundaries, we could use the forward and backward difference operators and implicitly embed the boundary conditions in the equations. However, it is easier to just explicitly enforce the boundary conditions.
 
 
 ## Boundary conditions
 
-We consider first the region $[0, x_s]$. The following also applies symmetrically to the region $[x_s, 1]$.
+At the ice cap $x_s$, we have $T=T_s=0^\circ C$ and at the pole and equator, we use Neumann boundary conditions $\frac{\partial T}{\partial x} = 0$. This can be discretized as $T_0 = T_1$ in the first domain and $T_N = T_{N-1}$ in the second domain.
 
-At the ice cap $x_s$, we have $T=T_s=273.15 K$ and at the pole and equator, we Neumann boundary conditions $\frac{\partial T}{\partial x} = 0$. This can be discretized as $T_0 = T_1$. This simplifies the second equation to
+### Higher order boundary conditions
 
-$$-D\frac{1-x_1^2}{\Delta x} \frac{T_2-T_1}{\Delta x} = -I_0 + Q S_0 a_0$$
+The central difference operator is of second dergree, but that helps little, when the boundary conditions are of first degree. A second order approximation of the Neumann boundary conditions would be
+
+$$3T_0 - 4T_1 + T_2 = 0$$
+
+and
+
+$$3T_N - 4T_{N-1} + T_{N-2} = 0.$$
 
 
 ## Matrix Reformulation
 
-In the interior, the equations can be rewritten as
+We first consider the domain $[0,x_s]$. In the interior, the equations can be rewritten as
 
 $$\alpha_j T_{j-1} + \beta_j T_j + \gamma_j T_{j+1} = f_j,$$
 
@@ -75,44 +75,29 @@ $$
 \end{aligned}
 $$
 
-At the equator, we have
-
-$$\tilde\alpha_0 T_0 + \tilde\beta_0 T_1 + \tilde\gamma_0 T_2 = f_0,$$
-
-with $f_0$ as above and
-
-$$
-\begin{aligned}
-    \tilde\alpha_0 &= B_\text{out} \\
-    \tilde\beta_0 &= D \frac{1-x_1^2}{\Delta x^2}, \\
-    \tilde\gamma_0 &= - D \frac{1-x_1^2}{\Delta x^2}.
-\end{aligned}
-$$
-
-At the ice cap, we have
-
-$$\hat\alpha_N T_{N-2} + \hat\beta_N T_{N-1} = \hat f_N,$$
-
-with
-
-$$
-\begin{aligned}
-    \hat\alpha_N &= - D \frac{1-x_{N-1}^2}{\Delta x^2}, \\
-    \hat\beta_N &= D \frac{2-x_s^2-x_{N-1}^2}{\Delta x^2}, \\
-    \hat f_N &= \left(D \frac{1-x_s^2}{\Delta x^2} - B_\text{out}\right) T_s - A_\text{out} + Q S_N a_N.
-\end{aligned}
-$$
-
-We can now write this system of equations as
+Additionally, we apply the boundary conditions given above. We can now write this system of equations as
 
 $$A \mathbf{T} = \mathbf{f},$$
 
-where $\mathbf{T} = [T_0, T_1, \ldots, T_N]^T$, $\mathbf{f} = [f_0, f_1, \ldots, f_{N-1}, \hat f_N]^T$, and 
+where $\mathbf{T} = [T_0, T_1, \ldots, T_N]^T$, $\mathbf{f} = [f_0, f_1, \ldots, f_{N-1}, f_N]^T$, 
 
 $$A = \begin{bmatrix}
-    \tilde\alpha_0 & \tilde\beta_0 & \tilde\gamma_0 & 0 & 0 &\cdots & 0 & 0 \\
-    \alpha_1 & \beta_1 & \gamma_1 & 0 & \cdots & 0 & 0 & 0 \\
+    3 & -4 & 1 & 0 &\cdots & 0 & 0 & 0 & 0 \\
+    \alpha_1 & \beta_1 & \gamma_1 & 0 & \cdots & 0 & 0 & 0 & 0 \\
+    0 & \alpha_2 & \beta_2 & \gamma_2 & \cdots & 0 & 0 & 0 & 0 \\
     & & & &\ddots & & & \\
-    0 & 0 & 0 & 0 & \cdots & \alpha_{N-1} & \beta_{N-1} & \gamma_{N-1} \\
-    0 & 0 & 0 & 0 & \cdots & \hat\alpha_N & \hat\beta_N & 0
-\end{bmatrix}$$
+    0 & 0 & 0 & 0 & \cdots & 0 & \alpha_{N-1} & \beta_{N-1} & \gamma_{N-1} \\
+    0 & 0 & 0 & 0 & \cdots & 0 & 0 & 0 & 1
+\end{bmatrix},$$
+
+and $f_0 = \frac{\partial T}{\partial x}=0$ and $f_N = T_s$ enforce the boundary conditions.
+
+
+Above the ice cap, $[x_s, 1]$, due to symmetry, we just flip theindices of $\mathbf{f}$ and $A$:
+
+$$
+\begin{aligned}
+    f_j &\gets f_{N+1-j} \\
+    A_{j,k} &\gets A_{N+1-j, N+1-k}
+\end{aligned}
+$$
--- 10/numerical_scheme.py
+++ 10/numerical_scheme.py
@@ -1,82 +1,137 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as anim
 
-def create_diagonal(N, dx, x, D, B_out):
+def compute_diagonal(N, dx, x, D, B_out):
     """
-    Create the diagonal of the matrix for the numerical scheme.
+    compute the diagonal of the matrix for the numerical scheme.
     """
-    diag = np.empty(N+1)
-    diag[0] = B_out
-    diag[-1] = 0
+    diag = np.zeros(N+1)
     diag[1:-1] = D * (2 - (x[1:-1]-0.5*dx)**2 - (x[1:-1]+0.5*dx)**2) / dx**2 + B_out
     return diag
 
 
-def create_lower(N, dx, x, D):
+def compute_lower(N, dx, x, D):
     """
-    Create the lower diagonal of the matrix for the numerical scheme.
+    compute the lower diagonal of the matrix for the numerical scheme.
     """
-    lower = np.empty(N)
+    lower = np.zeros(N)
     lower[:-1] = -D * (1 - (x[1:-1]-0.5*dx)**2) / dx**2
-    lower[-1] = D * (2 - x[-1]**2 - x[-2]**2) / dx**2
     return lower
 
 
-def create_upper(N, dx, x, D):
+def compute_upper(N, dx, x, D):
     """
-    Create the upper diagonal of the matrix for the numerical scheme.
+    compute the upper diagonal of the matrix for the numerical scheme.
     """
-    upper = np.empty(N)
-    upper[0] = D * (1 - x[1]**2) / dx**2
+    upper = np.zeros(N)
     upper[1:] = -D * (1 - (x[1:-1]+0.5*dx)**2) / dx**2
     return upper
 
 
-def create_matrix(N, dx, x, D, B_out):
+def compute_south_matrix(N, dx, x, D, B_out):
     """
-    Create a matrix for the numerical scheme.
+    compute a matrix for the numerical scheme.
     """
-    lower = create_lower(N, dx, x, D)
-    upper = create_upper(N, dx, x, D)
-    diag = create_diagonal(N, dx, x, D, B_out)
+    lower = compute_lower(N, dx, x, D)
+    upper = compute_upper(N, dx, x, D)
+    diag = compute_diagonal(N, dx, x, D, B_out)
     A = np.diag(diag) + np.diag(lower, -1) + np.diag(upper, 1)
-    A[0,2] = -D * (1 - x[1]**2) / dx**2
-    A[-1,-3] = -D * (1 - x[-2]**2) / dx**2
-
+    enforce_boundary_condition_south_matrix(A)
     return A
 
+def enforce_boundary_condition_south_matrix(A):
+    A[0, 0:3] = [3, -4, 1] # Neumann condition, second order
+    # A[0, 0:2] = [1, -1] # Neumann condition, first order
+    A[-1,-1] = 1            # Dirichlet condition
 
-def create_rhs(N, dx, x, D, A_out, B_out, Q, S, a, T_s):
+def enforce_boundary_condition_south_rhs(f, T_s):
+    f[0] = 0  # Neumann condition
+    f[-1] = T_s # Dirichlet condition
+
+def compute_north_matrix(N, dx, x, D, B_out):
+    return np.flip(compute_south_matrix(N, dx, x, D, B_out))
+
+
+def compute_south_rhs(N, x, A_out, Q, S, a, T_s):
     """
-    Create the right-hand side of the numerical scheme.
+    compute the right-hand side of the numerical scheme.
     """
-    rhs = -A_out + Q * S(x) * a
-    # rhs = A_out + Q * S(x) * a # Flipping the sign in front of A_out giver reasonable results, but must be wrong
-    rhs[-1] += (D * (1 - x[-1]**2) / dx**2 - B_out) * T_s
+    rhs = np.empty(N+1)
+    rhs[1:-1] = -A_out + Q * S(x[1:-1]) * a
+    enforce_boundary_condition_south_rhs(rhs, T_s)
     return rhs
 
+def compute_north_rhs(N, x, A_out, Q, S, a, T_s):
+    return np.flip(compute_south_rhs(N, x, A_out, Q, S, a, T_s))
+
+
+def S(x, S_2=-0.477):
+    return 1 + 0.5*S_2 * (3*x**2 - 1)
+
+def compute_temperature(N, x_s):
+    N_south = int(N * x_s)
+    N_north = (N - N_south)*10
+    x_south, dx_south = np.linspace(0, x_s, N_south+1, retstep=True)
+    x_north, dx_north = np.linspace(x_s, 1, N_north+1, retstep=True)
+    D = 0.3
+    a_south = 0.68
+    a_north = 0.38
+    A_out = 201.4
+    B_out = 1.45
+    T_s = 0
+    Q = 340
+
+    f_south = compute_south_rhs(N_south, x_south, A_out, Q, S, a_south, T_s)
+    A_south = compute_south_matrix(N_south, dx_south, x_south, D, B_out)
+    T_south = np.linalg.solve(A_south, f_south)
+
+    f_north = compute_north_rhs(N_north, x_north, A_out, Q, S, a_north, T_s)
+    A_north = compute_north_matrix(N_north, dx_north, x_north, D, B_out)
+    T_north = np.linalg.solve(A_north, f_north)
+
+    return T_south, x_south, T_north, x_north
 
 
 def main():
-    N = 50
-    x_s = 0.6
-    x, dx = np.linspace(0, x_s, N+1, retstep=True)
-    D = 0.3
-    a = 0.68
-    A_out = 201.4
-    B_out = 1.45
-    T_s = 273.15
-    Q = 340
-    def S(x, S_2=-0.477):
-        return 1 + 0.5*S_2 * (3*x**2 - 1)
+    N = 200
+    x_s = 0.9
 
-    f = create_rhs(N, dx, x, D, A_out, B_out, Q, S, a, T_s)
-    A = create_matrix(N, dx, x, D, B_out)
-    T = np.linalg.solve(A, f)
-
-    print(T)
+    fig, ax = plt.subplots()
+    T_south, x_south, T_north, x_north = compute_temperature(N, x_s)
     
-    plt.plot(x, T)
+    line_south, = ax.plot(np.asin(x_south), T_south, label='South')
+    line_north, = ax.plot(np.asin(x_north), T_north, label='North')
+    ice_line = ax.axvline(np.arcsin(x_s), color='black', linestyle='--', label='Ice edge')
+    zero_line = ax.axhline(0, color='black', linestyle='--')
+    
+    ax.set_xlabel('$\\varphi/\\text{rad}$')
+    ax.set_ylabel('$T/C^{\circ}$')
+    ax.legend()
+
+    # plt.show()
+
+    frames = 75
+    x_low = 0.9
+    x_high = 0.999
+
+    def update(frame):
+        # x_s_frame = x_s + (1+np.cos(2*np.pi*frame/frames))**2 * (1-2*x_s)
+        phi_s_frame = np.asin(x_low) + (np.asin(x_high) - np.asin(x_low)) * (1 - abs(frame/frames - 1)**2)
+        x_s_frame = np.sin(phi_s_frame)
+
+        T_south, x_south, T_north, x_north = compute_temperature(N, x_s_frame)
+        
+        line_south.set_data(np.asin(x_south), T_south)
+        line_north.set_data(np.asin(x_north), T_north)
+        ice_line.set_xdata(np.arcsin(x_north[[0, 0]]))
+
+        return line_south, line_north, ice_line
+
+    ani = anim.FuncAnimation(fig, update, frames=2*frames, blit=True, repeat=True, interval=20)
+    # writer = anim.PillowWriter(fps=30,
+    #                            bitrate=1800)
+    # ani.save('animation_70deg.gif', writer=writer)
     plt.show()
 
 if __name__ == '__main__':
